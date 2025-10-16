bef67695a486f1405e5e13a5623514cfc61765f0
felipeFleite <felipefracalossileite@gmail.com>
2025-10-15 23:45:40 -0400

    frontend css/ login.jsx

--- FrontEnd/vite-project/src/main.jsx
+++ FrontEnd/vite-project/src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '../css/index.css'
 import Cadastro from './Cadastro.jsx'
+import Login from './Login.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Cadastro />
+    <Login />
   </StrictMode>,
 )
